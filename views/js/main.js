6ad29ea2a01eda972adbe0ae82a7ceaa2ddefae7
Sandra <33291091+Sandra-Rae7@users.noreply.github.com>
2018-12-04 14:13:16 -0600

    Update main.js
    Moved ScrollTop outside of the loop to see if it speeds up the page...

--- views/js/main.js
+++ views/js/main.js
@@ -502,12 +502,12 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  // document.body.scrollTop is no longer supported in Chrome.
+  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  
   for (var i = 0; i < items.length; i++) {
-    // document.body.scrollTop is no longer supported in Chrome.
-    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
     var phase = Math.sin((scrollTop / 1250) + (i % 5));
-    var basicLeft = items[i].basicLeft;
-    items[i].style.left = basicLeft + 100 * phase + 'px';
+    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
