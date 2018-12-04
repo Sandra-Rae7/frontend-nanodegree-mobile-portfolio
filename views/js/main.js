d627b8710a357f90be030252fbe309510d80664b
Sandra <33291091+Sandra-Rae7@users.noreply.github.com>
2018-12-04 13:46:50 -0600

    Update main.js
    Created a basicLeft variable to maybe speed up the calculation...

--- views/js/main.js
+++ views/js/main.js
@@ -506,7 +506,8 @@ function updatePositions() {
     // document.body.scrollTop is no longer supported in Chrome.
     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
     var phase = Math.sin((scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    var basicLeft = items[i].basicLeft;
+    items[i].style.left = basicLeft + 100 * phase + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
